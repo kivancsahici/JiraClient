fbaf238570ee185a52eaf9ff2b3044f5b63fcb49
Kıvanç Sahici <kivancsahici@yahoo.com>
2018-06-10 14:41:45 +0300

    stupid approach to fill navigation view with data after user is logged in

--- src/com/acme/rcp/action/LoginDialogAction.java
+++ src/com/acme/rcp/action/LoginDialogAction.java
@@ -2,11 +2,14 @@ package com.acme.rcp.action;
 
 import huseyin.ICommandIds;
 import huseyin.LoginDialog;
+import huseyin.NavigationView;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.window.Window;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 
 public class LoginDialogAction extends Action {
@@ -23,6 +26,14 @@ public class LoginDialogAction extends Action {
 	  //dialog.open();
 	  if (dialog.open() != Window.OK) {
 	        System.out.println("user pressed cancel");
+	  }
+	  else {
+		  IWorkbenchPage page = window.getActivePage();
+		  //IViewPart view = page.showView(MainView.ID)  //id de la view in plugin.xml
+		  //page.hideView(page.findView(SitesView.ID));
+		  NavigationView view = (NavigationView)page.findView(NavigationView.ID);
+		  view.setInput();
+		 
 	  }
       //.openInformation(window.getShell(), "Open", "Open Message Dialog!");
   }
